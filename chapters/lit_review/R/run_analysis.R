46eb4f8425bd816515d3857580add367c599c66e
Steven Hawes <maxhawes@gmail.com>
2017-03-08 17:22:17 +0800

    Drafted the introduction and methods sections

--- chapters/lit_review/R/run_analysis.R
+++ chapters/lit_review/R/run_analysis.R
@@ -14,6 +14,7 @@ library("ggplot2")
 # Load external functions
 source("sort_factor.R")
 source("get_factor_proportion.R")
+source("multiplot.R")
 
 # Clean the data sources after running
 clean <- FALSE
@@ -69,20 +70,89 @@ summary(data.all$model_time_step)
 ## Comparisons with metrics section
 
 ### PHYSICAL COMPARISONS
+#BPM model
+data.all$model_name <- as.factor(data.all$model_name)
+plot1 <- ggplot(data.all,aes(model_name,self_recruitment_mean)) + geom_boxplot()+ geom_jitter(width = 0.2) + coord_flip()
+plot2 <- ggplot(data.all,aes(model_name,local_retention_mean)) + geom_boxplot()+ geom_jitter(width = 0.2) + coord_flip()
+plot3 <- ggplot(data.all,aes(model_name,settlement_success_mean)) + geom_boxplot()+ geom_jitter(width = 0.2) + coord_flip()
+plot4 <- ggplot(data.all,aes(model_name,distance_travelled_mean)) + geom_boxplot()+ geom_jitter(width = 0.2) + coord_flip()
+model.biological_plot <- multiplot(g1,g2,g3,g4, cols=2)
+ggsave("../figs/model_bdm_metrics.png",plot=model.biological_plot)
 
+#Ocean model
+data.all$physical_model <- as.factor(data.all$physical_model)
+plot1 <- ggplot(data.all,aes(physical_model,self_recruitment_mean)) + geom_boxplot()+ geom_jitter(width = 0.2) + coord_flip()
+plot2 <- ggplot(data.all,aes(physical_model,local_retention_mean)) + geom_boxplot()+ geom_jitter(width = 0.2) + coord_flip()
+plot3 <- ggplot(data.all,aes(physical_model,settlement_success_mean)) + geom_boxplot()+ geom_jitter(width = 0.2) + coord_flip()
+plot4 <-ggplot(data.all,aes(physical_model,distance_travelled_mean)) + geom_boxplot()+ geom_jitter(width = 0.2) + coord_flip()
+model.physical_plot <- multiplot(g1,g2,g3,g4, cols=2)
+ggsave("../figs/model_physical_metrics.png",plot=model.physical_plot)
+
+# Oceanic region
+data.all$oceanic_region <- as.factor(data.all$oceanic_region)
+plot1 <- ggplot(data.all,aes(oceanic_region,self_recruitment_mean)) + geom_boxplot()+ geom_jitter(width = 0.2) + coord_flip()
+plot2 <- ggplot(data.all,aes(oceanic_region,local_retention_mean)) + geom_boxplot()+ geom_jitter(width = 0.2) + coord_flip()
+plot3 <- ggplot(data.all,aes(oceanic_region,settlement_success_mean)) + geom_boxplot()+ geom_jitter(width = 0.2) + coord_flip()
+plot4 <-ggplot(data.all,aes(oceanic_region,distance_travelled_mean)) + geom_boxplot()+ geom_jitter(width = 0.2) + coord_flip()
+ocean.plot <- multiplot(plot1,plot2,plot3,plot4, cols=2)
+ggsave("../figs/oceanic_metrics.png",plot=ocean.plot)
+
+# System choice
+data.all$system <- as.factor(data.all$system)
+bartlett.test(self_recruitment_mean ~ system, data=data.all)
+t.test(self_recruitment_mean ~ system, data=data.all, var.equal=FALSE)
+bartlett.test(local_retention_mean ~ system, data=data.all)
+t.test(local_retention_mean ~ system, data=data.all, var.equal=FALSE)
+bartlett.test(settlement_success_mean ~ system, data=data.all)
+t.test(settlement_success_mean ~ system, data=data.all, var.equal=TRUE)
+bartlett.test(distance_travelled_mean ~ system, data=data.all)
+t.test(distance_travelled_mean ~ system, data=data.all, var.equal=FALSE)
+
+
+# Model resolution
+resolution.data <- data.all %>% select(model_resolution_min,self_recruitment_mean,settlement_success_mean,
+                                       local_retention_mean,distance_travelled_mean)
+resolution.sr_lm <- lm(model_resolution_min ~ self_recruitment_mean, data = resolution.data)
+summary(resolution.sr_lm)
+resolution.ss_lm <- lm(model_resolution_min ~ settlement_success_mean, data = resolution.data)
+summary(resolution.ss_lm)
+resolution.lr_lm <- lm(model_resolution_min ~ local_retention_mean, data = resolution.data)
+summary(resolution.lr_lm)
+resolution.dd_lm <- lm(model_resolution_min ~ distance_travelled_mean, data = resolution.data)
+summary(resolution.dd_lm)
+
+#Nested models
+bartlett.test(self_recruitment_mean ~ nested_submodels, data=data.all)
+t.test(self_recruitment_mean ~ nested_submodels, data=data.all, var.equal=FALSE)
+#bartlett.test(local_retention_mean ~ nested_submodels, data=data.all)
+#t.test(local_retention_mean ~ nested_submodels, data=data.all, var.equal=FALSE)
+bartlett.test(settlement_success_mean ~ nested_submodels, data=data.all)
+t.test(settlement_success_mean ~ nested_submodels, data=data.all, var.equal=TRUE)
+bartlett.test(distance_travelled_mean ~ nested_submodels, data=data.all)
+t.test(distance_travelled_mean ~ nested_submodels, data=data.all, var.equal=FALSE)
+
+       
 ### BIOLOGICAL COMPARISONS
-
-
+datastuff <- select(data.all$nested_submodels,data.all$self_recruitment_mean)
 
 #Settlement compentency window
 
 #Add the window
 data.all <- data.all %>% mutate(window=pld_fixed-settlement_competency_type_start)
 #ggplot(data.all,aes(window,self_recruitment_mean)) + geom_point() + geom_smooth(method=lm)
-window.sr_correlation <- cor.test(data.all$window,data.all$self_recruitment_mean)
-window.sr_correlation <- cor.test(data.all$window,data.all$local_retention_mean)
-window.ss_correlation <- cor.test(data.all$window,data.all$settlement_success_mean)
-window.dt_correlation <- cor.test(data.all$window,data.all$distance_travelled_mean)
+window.sr_lm <- lm(data.all$window,data.all$self_recruitment_mean)
+window.sr_lm <- cor.test(data.all$window,data.all$local_retention_mean)
+window.ss_lm <- cor.test(data.all$window,data.all$settlement_success_mean)
+window.dt_lm <- cor.test(data.all$window,data.all$distance_travelled_mean)
+
+window.sr_lm <- lm(window ~ self_recruitment_mean, data = data.all)
+window.ss_lm <- lm(window ~ settlement_success_mean, data = data.all)
+window.lr_lm <- lm(window ~ local_retention_mean, data = data.all)
+window.dd_lm <- lm(window ~ distance_travelled_mean, data = data.all)
+summary(window.sr_lm)
+summary(window.ss_lm)
+summary(window.lr_lm)
+summary(window.dd_lm)
 
 data.window <- data.all %>% select(PLD_fixed,Settlement_competency_type_start,Settlement_success_mean) %>%
   mutate(window=PLD_fixed-Settlement_competency_type_start) %>%
