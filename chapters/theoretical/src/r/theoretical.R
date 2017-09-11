9074d8d47ffc8c1f1cc4595e6b77871067868c4f
Steve Hawes <maxhawes@gmail.com>
2017-09-11 23:21:02 +0800

    Added the connectivity matrix builder

--- chapters/theoretical/src/r/theoretical.R
+++ chapters/theoretical/src/r/theoretical.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
-# Read in the data
 
+# nMDS Analysis
 headings <- c("id","birthday","region","source","age","recruited","settle")
 
 # Read is the collection of files
@@ -13,7 +13,6 @@ passive.data$birthday <- as.Date(passive.data$birthday)
 passive.data$recruited <- as.Date(passive.data$recruited)
 passive.data <- as.tibble(passive.data)
 passive.data <- mutate(passive.data,days = age/86400)
-passive.connectivity <- summarize(group_by(passive.data,source, settle), count = n())
 passive.settlement <- summarize(group_by(passive.data, settle), passive = n())
 
 path <- c("/Users/steve/Downloads/")
@@ -52,11 +51,25 @@ orientate.settlement <- summarize(group_by(orientate.data, settle), orientate = 
 phase1.data <- passive.settlement %>% full_join(diel.settlement) %>% full_join(ovm.settlement) %>% full_join(orientate.settlement)
 phase1.community_matrix <- data.matrix(phase1.community_matrix)
 row.names(phase1.community_matrix) <- phase1.data$settle
+phase1.community_matrix <- phase1.community_matrix[,2:3]
 
 # nMDS
 phase1.nmds <- metaMDS(phase1.community_matrix, k=2, trymax=100)
 stressplot(phase1.nmds)
 plot(phase1.nmds)
 
+# Connectivity analysis
+reefs <- read_csv("../../data/reefs.csv")
+#passive.connectivity <- summarize(group_by(passive.data,source, settle), count = n())
+passive.connectivity <- left_join(passive.data, reefs,by=c("settle"="PATCH_NUM"))
+passive.connectivity <- rename(passive.connectivity, settle.region=REGION)
+passive.connectivity <- left_join(passive.connectivity, reefs,by=c("source"="PATCH_NUM"))
+passive.connectivity <- rename(passive.connectivity, source.region=REGION)
+passive.region <- summarize(group_by(passive.connectivity,source.region, settle.region), count = n())
+passive.region <- spread(passive.region,key=settle.region,value=count)
+passive.region_matrix <- data.matrix(passive.region)
+row.names(passive.region_matrix) <- passive.region$source.region
+passive.region_matrix <- passive.region_matrix[,-1]
+passive.reef_matrix <- summarize(group_by(passive.connectivity,source, settle), count = n())
 
 
