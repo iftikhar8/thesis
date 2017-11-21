16833c6aef808736debe988c8fa64145ecac9a28
Steven Hawes <maxhawes@gmail.com>
2017-11-21 14:15:45 +0800

    Scripts and configs for applied chapter

---
+++ chapters/applied/src/scripts/copy-netcdf.sh
@@ -0,0 +1,13 @@
+cp ../bran3p5_data/w/ocean_w_2011_04.nc ../bran3p5_data/w/ocean_w_2011_05.nc  ../bran3p5_data/w/ocean_w_2011_06.nc ../bran3p5_data/w/ocean_w_2011_07.nc ../bran3p5_data/w/ocean_w_2011_08.nc 2011/w/.
+
+cp ../bran3p5_data/v/ocean_v_2011_04.nc ../bran3p5_data/v/ocean_v_2011_05.nc  ../bran3p5_data/v/ocean_v_2011_06.nc ../bran3p5_data/v/ocean_v_2011_07.nc ../bran3p5_data/v/ocean_v_2011_08.nc 2011/v/.
+
+cp ../bran3p5_data/w/ocean_w_2011_04.nc ../bran3p5_data/w/ocean_w_2011_05.nc  ../bran3p5_data/w/ocean_w_2011_06.nc ../bran3p5_data/w/ocean_w_2011_07.nc ../bran3p5_data/w/ocean_w_2011_08.nc 2011/w/.
+
+cp ../bran3p5_data/w/ocean_w_2010_04.nc ../bran3p5_data/w/ocean_w_2010_05.nc  ../bran3p5_data/w/ocean_w_2010_06.nc ../bran3p5_data/w/ocean_w_2010_07.nc ../bran3p5_data/w/ocean_w_2010_08.nc 2010/w/.
+
+cp ../bran3p5_data/v/ocean_v_2010_04.nc ../bran3p5_data/v/ocean_v_2010_05.nc  ../bran3p5_data/v/ocean_v_2010_06.nc ../bran3p5_data/v/ocean_v_2010_07.nc ../bran3p5_data/v/ocean_v_2010_08.nc 2010/v/.
+
+cp ../bran3p5_data/w/ocean_w_2010_04.nc ../bran3p5_data/w/ocean_w_2010_05.nc  ../bran3p5_data/w/ocean_w_2010_06.nc ../bran3p5_data/w/ocean_w_2010_07.nc ../bran3p5_data/w/ocean_w_2010_08.nc ../bran3p5_data/w/ocean_w_2010_09.nc 2010/w/.
+
+rsync -avx shaw7955@rcos-int.sydney.edu.au:/rds/PRJ-ichthyconnect/netcdf/blackcod/2010 /srv/zissou/input/netcdf/bran/.
