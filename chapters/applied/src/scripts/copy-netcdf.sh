cbe0b38e0a68f4b1df4ce5c3c1f53af80884d092
Steven Hawes <maxhawes@gmail.com>
2017-11-22 23:56:22 +0800

    Added scripts for the applied chapter

--- chapters/applied/src/scripts/copy-netcdf.sh
+++ chapters/applied/src/scripts/copy-netcdf.sh
@@ -1,13 +1,8 @@
-cp ../bran3p5_data/w/ocean_w_2011_04.nc ../bran3p5_data/w/ocean_w_2011_05.nc  ../bran3p5_data/w/ocean_w_2011_06.nc ../bran3p5_data/w/ocean_w_2011_07.nc ../bran3p5_data/w/ocean_w_2011_08.nc 2011/w/.
+cp ../bran3p5_data/u/ocean_u_2004_04.nc ../bran3p5_data/u/ocean_u_2004_05.nc  ../bran3p5_data/u/ocean_u_2004_06.nc ../bran3p5_data/u/ocean_u_2004_07.nc ../bran3p5_data/u/ocean_u_2004_08.nc 2004/u/.
 
-cp ../bran3p5_data/v/ocean_v_2011_04.nc ../bran3p5_data/v/ocean_v_2011_05.nc  ../bran3p5_data/v/ocean_v_2011_06.nc ../bran3p5_data/v/ocean_v_2011_07.nc ../bran3p5_data/v/ocean_v_2011_08.nc 2011/v/.
+cp ../bran3p5_data/v/ocean_v_2004_04.nc ../bran3p5_data/v/ocean_v_2004_05.nc  ../bran3p5_data/v/ocean_v_2004_06.nc ../bran3p5_data/v/ocean_v_2004_07.nc ../bran3p5_data/v/ocean_v_2004_08.nc 2004/v/.
 
-cp ../bran3p5_data/w/ocean_w_2011_04.nc ../bran3p5_data/w/ocean_w_2011_05.nc  ../bran3p5_data/w/ocean_w_2011_06.nc ../bran3p5_data/w/ocean_w_2011_07.nc ../bran3p5_data/w/ocean_w_2011_08.nc 2011/w/.
+cp ../bran3p5_data/w/ocean_w_2004_04.nc ../bran3p5_data/w/ocean_w_2004_05.nc  ../bran3p5_data/w/ocean_w_2004_06.nc ../bran3p5_data/w/ocean_w_2004_07.nc ../bran3p5_data/w/ocean_w_2004_08.nc 2004/w/.
 
-cp ../bran3p5_data/w/ocean_w_2010_04.nc ../bran3p5_data/w/ocean_w_2010_05.nc  ../bran3p5_data/w/ocean_w_2010_06.nc ../bran3p5_data/w/ocean_w_2010_07.nc ../bran3p5_data/w/ocean_w_2010_08.nc 2010/w/.
-
-cp ../bran3p5_data/v/ocean_v_2010_04.nc ../bran3p5_data/v/ocean_v_2010_05.nc  ../bran3p5_data/v/ocean_v_2010_06.nc ../bran3p5_data/v/ocean_v_2010_07.nc ../bran3p5_data/v/ocean_v_2010_08.nc 2010/v/.
-
-cp ../bran3p5_data/w/ocean_w_2010_04.nc ../bran3p5_data/w/ocean_w_2010_05.nc  ../bran3p5_data/w/ocean_w_2010_06.nc ../bran3p5_data/w/ocean_w_2010_07.nc ../bran3p5_data/w/ocean_w_2010_08.nc ../bran3p5_data/w/ocean_w_2010_09.nc 2010/w/.
 
 rsync -avx shaw7955@rcos-int.sydney.edu.au:/rds/PRJ-ichthyconnect/netcdf/blackcod/2010 /srv/zissou/input/netcdf/bran/.
